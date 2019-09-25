3360295a4dc1596eb42b402021842eb80eef8dac
amisha237 <47049886+amisha237@users.noreply.github.com>
2019-09-26 01:52:40 +0530

    users page ,userform added

---
+++ components/UserForm.vue
@@ -0,0 +1,113 @@
+<template>
+    <v-layout wrap class=" align-center justify-center">
+    <v-flex xs12 sm12 md9 lg9>
+      <v-card class="mx-auto pa-10" light max-width="800">
+
+         <v-card-title class="align-center justify-center display-1 mb-2">User Form</v-card-title>
+         <v-form
+            ref="form"
+            v-model="active"
+        >
+        <!-- name -->
+           <v-text-field
+            v-model="name"
+            :rules="nameRules"
+            label="Name"
+            required
+            outlined
+            ></v-text-field>
+         <!-- email -->
+            <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+            outlined
+            ></v-text-field>
+        <!-- gender -->
+        <span > Gender </span>
+         <v-radio-group v-model="radioGroup" outlined>
+            <v-radio
+             label="Male"
+             ></v-radio>
+              <v-radio
+             label="Female"
+             ></v-radio>
+              <v-radio
+             label="Others"
+             ></v-radio>
+        </v-radio-group>
+
+
+        <!-- contact number -->
+            <v-text-field
+            v-model="contact"
+            label="Contact Number"
+            required
+            outlined
+            ></v-text-field>
+
+        <!-- address -->
+             <v-text-field
+            v-model="address"
+            label="Address"
+            outlined
+            ></v-text-field>
+
+        <!-- city -->
+            <v-text-field
+            v-model="city"
+            label="City"
+            outlined
+            ></v-text-field>
+        
+        <!-- pin -->
+            <v-text-field
+            v-model="pin"
+            label="PIN"
+            outlined
+            ></v-text-field>
+
+         <!--submit  -->
+            <div class="text-center">
+            <v-btn rounded  large class="primary mr-3"> Submit </v-btn>
+            <v-btn color="error" @click="reset" large rounded>  Clear  </v-btn>
+            </div>
+           
+        </v-form>
+      
+
+      </v-card>
+      </v-flex>
+    </v-layout>   
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      active: true,
+      name: '',
+       nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 30) || 'Name must be less than 30 characters',
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      radioGroup:'',
+      contact:'',
+      address:'',
+      city:'',
+      pin:'',
+      
+    }),
+
+    methods: {
+      reset () {
+        this.$refs.form.reset()
+      },
+  },
+}
+</script>
---
+++ pages/configure/users.vue
@@ -0,0 +1,108 @@
+<template>
+  <v-card
+    class="mx-auto mt-10"
+    max-width="1000"
+  >
+      <v-card-title class="align justify-end ">
+        <v-flex xs12 sm12 md8 lg8>
+            <v-text-field
+             v-model="search"
+            placeholder="Search"
+            >
+             </v-text-field>
+       </v-flex>
+        <v-flex xs6 sm6 md2 lg2>
+            <v-btn class="ml-3 error"> Delete </v-btn>
+        </v-flex>
+        <!-- <v-flex xs6 sm6 md2 lg2>
+            <v-btn class="primary" >  Add New </v-btn>
+        </v-flex> -->
+
+        <template>
+            <v-dialog v-model="dialog"  max-width="1000px" >
+                <template v-slot:activator="{ on }">
+                    <v-flex xs6 sm6 md2 lg2>  
+                     <v-btn color="primary"  v-on="on">Add New</v-btn>
+                    </v-flex>
+                </template>
+                <UserForm />
+            </v-dialog>
+        </template>
+
+ </v-card-title>
+    <v-card>
+     <v-data-table
+      :headers="headers"
+      :items="users"
+      :search="search"
+     >
+     </v-data-table>
+  </v-card>
+
+   
+  </v-card>
+</template>
+<script>
+import UserForm from "~/components/UserForm"
+  export default {
+    components:{
+        UserForm
+    },
+    data () {
+      return {
+        search: '',
+        headers: [
+          {
+            text: 'ID',
+            align: 'left',
+            sortable: false,
+            value: 'id',
+          },
+          { text: 'Name', value: 'name' },
+          { text: 'Email', value: 'email' },
+          { text: 'Contact', value: 'contact' },
+          
+        ],
+        users: [
+          {
+            id:1,
+            name:'abc',
+            email:'abc',
+            contact:234,
+          },
+          {
+            id:2,
+            name:'abc',
+            email:'bcd',
+            contact:234,
+          },
+          {
+            id:3,
+            name:'abs',
+            email:'cvfg',
+            contact:234,
+          },
+          {
+            id:4,
+            name:'dfg',
+            email:'dfg',
+            contact:234,
+          },
+          {
+            id:5,
+            name:'xys',
+            email:'xys',
+            contact:234,
+          },
+          {
+            id:6,
+            name:'hjj',
+            email:'hjk',
+            contact:234,
+          },
+          
+        ],
+      }
+    },
+  }
+</script>
