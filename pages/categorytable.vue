1b1f451abcd9828d63db7e88bce2c5c0eff50aea
saumyagoyal1708 <1705071@kiit.ac.in>
2019-10-16 13:09:23 +0530

    category table

--- pages/categorytable.vue
+++ pages/categorytable.vue
@@ -20,7 +20,7 @@
                         <v-spacer></v-spacer>
                         <v-dialog v-model="dialog" max-width="500px">
                             <template v-slot:activator="{ on }">
-                                <v-btn color="primary" dark class="mb-2" v-on="on">Add Item</v-btn>
+                                <v-btn color="primary" dark class="mb-2" v-on="on">Add Category</v-btn>
                             </template>
                             <v-card>
                                 <v-card-title>
